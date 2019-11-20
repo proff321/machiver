8df640d902bcdd79127a4c168ded0ae74b785725
Eric Credeur <eric@ericcredeur.com>
2019-11-19 20:24:09 -0600

    Update Media to accept and validate file path

--- src/Media.php
+++ src/Media.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This class is used as the base class for all media types.  It madates the
+ * This class is used as the base class for all media types.  It mandates the
  * functions which are required by other management classes within the
  * application.
  *
@@ -11,12 +11,56 @@ declare(strict_types=1);
 
 namespace Machiver;
 
+use Exception;
+
 abstract class Media
 {
+    /** @var string The file path for this media object */
+    protected $filePath;
+
+    /** @var resource File handle for the media object at the provided file path */
+    protected $fileHandle;
+
+    /**
+     * Create a new media object with the data stored at the provided file path
+     *
+     * This will attempt to open the file at the provided location and will throw
+     * an exception if the utility cannot open the media.
+     *
+     * @throws Exception When the file path is empty
+     * @throws Exception When the file cannot be opened
+     */
+    public function __construct(string $filePath, string $mode = 'r')
+    {
+        if (empty($filePath)) {
+            throw new Exception('File path cannot be blank');
+        }
+
+        $this->filePath = $filePath;
+
+        if (!($fileHandle = fopen($filePath, $mode))){
+            throw new Exception("Unable to open file at location: {$filePath}");
+        }
+
+        $this->fileHandle = $fileHandle;
+    }
+
     /**
      * Create a universally unique ID that can be used to identify the media.
      * This ID must be universally unique as it is used for various management
      * tasks such as logging the history of the media.
      */
     public abstract function uuid(): string;
+
+    /**
+     * Determine if the class is responsible for the provided media type.  This
+     * allows the utility find the correct class for the various media formats
+     * created by a capture device.
+     *
+     * Where possible the developer is encouraged to look for concrete details
+     * about the media instead of relying on file extension alone.  This will
+     * help to reduce collisions where multiple classes can handle a given type
+     * of media.
+     */
+    public abstract function isType(Media $media): bool;
 }
--- tests/MediaTest.php
+++ tests/MediaTest.php
@@ -8,19 +8,28 @@ namespace Machiver\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Machiver\Media;
+use Machiver\Tests\MediaConcrete;
+use Exception;
 
 class MediaTest extends TestCase
 {
     public function testShouldCreateExtensibleClass()
     {
-        $media = new class extends Media
-        {
-            public function uuid(): string
-            {
-                return '1234';
-            }
-        };
-
+        $media = new MediaConcrete(__DIR__ . '/data/testJpeg.jpeg');
         $this->assertNotEmpty($media);
+    }
+
+    public function testShouldThrowExceptionOnEmptyFilePath()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('cannot be blank');
+        $media = new MediaConcrete('');
+    }
+
+    public function testShouldThrowExceptionOnMissingFile()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Unable to open file at location: /invalid/file/path');
+        $media = @new MediaConcrete('/invalid/file/path');
     }
 }

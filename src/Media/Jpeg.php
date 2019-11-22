9914ad529cfb0ab05c251d64902fdbd757e4ee55
Eric Credeur <eric@ericcredeur.com>
2019-11-21 21:50:19 -0600

    Working file identification proof of concept

--- src/Media/Jpeg.php
+++ src/Media/Jpeg.php
@@ -23,6 +23,34 @@ class Jpeg extends Photo
      */
     public static function isType($fileHandle): bool
     {
+        // X'FF', SOI, X'FF', APP0, <2 bytes to be skipped>, "JFIF", X'00'.
+        // The SOI marker is first and is part of the ITU T-81 standard
+        $soiMarker = hex2bin('FFD8');
+
+        // Then we have the APP0 marker, also defined by the ITU T-81 standard
+        $appMarker = hex2bin('FFE0');
+
+        // skip two bites
+        // TODO:  Not sure how to do this right now, maybe use regex?
+
+        // Then the string 'JFIF' which is specified by the ___ standard
+        $formatFlag = 'JFIF';
+
+        // Null string terminator
+        $nullTerminator = hex2bin('00');
+
+        // TODO:  Use the stream_get_line method to clean this up
+        $fileContents = fread($fileHandle, 100000000);
+
+        $indicator = $soiMarker . $appMarker . $formatFlag . $nullString;
+
+        if (
+            strpos($fileContents, $soiMarker . $appMarker) !== false
+            && strpos($fileContents, $formatFlag . $nullTerminator) !== false
+        ) {
+            return true;
+        }
+
         return false;
     }
 }
